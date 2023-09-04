4bf0cc0527dd4647a51e5a1599f14df24f20b8be
ragu <rrrtechie@gmail.com>
2023-09-04 22:18:35 +0530

    better code

--- llama.py
+++ llama.py
@@ -5,17 +5,19 @@ import os
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
+# Replicate Credentials
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot')
     # if 'REPLICATE_API_TOKEN' in st.secrets:
     #     st.success('API key already provided!', icon='✅')
     #     replicate_api = st.secrets['REPLICATE_API_TOKEN']
     # else:
-    replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-        st.warning('Please enter your credentials!', icon='⚠️')
-    else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
+    # replicate_api = st.text_input('Enter Replicate API token:', type='password')
+    # if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
+    #     st.warning('Please enter your credentials!', icon='⚠️')
+    # else:
+    #     st.success('Proceed to entering your prompt message!', icon='👉')
+    replicate_api = "r8_RylgPiJn3R7qK9Bq3txiGu2Y6hOH0lv0URHrL"
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
@@ -26,7 +28,7 @@ with st.sidebar:
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=600, step=8)
     st.markdown(
         '📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
@@ -56,11 +58,10 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat'
-                           ':df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
-                           input={"prompt": f"{prompt_input} Assistant: ",
-                                  "temperature": temperature, "top_p": top_p, "max_length": max_length,
-                                  "repetition_penalty": 1})
+    output = replicate.run(
+        'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
+        input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+               "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
     return output
 
 
@@ -71,18 +72,15 @@ if prompt := st.chat_input(disabled=not replicate_api):
         st.write(prompt)
 
 # Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "asgit remote -v, sistant":
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
             for item in response:
-                print(item)
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
-
-
