11fd086043a8f8224afed0c66a7e75b432848253
ragu <rrrtechie@gmail.com>
2023-09-13 08:01:23 +0530

    app

--- llama.py
+++ llama.py
@@ -4,9 +4,6 @@ import os
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
-st.write(
-    'If you are facing issues please press Clear Chat History button in the sidebar and retype your question '
-    'again , Thank You ')
 
 # Replicate Credentials
 with st.sidebar:
@@ -15,12 +12,12 @@ with st.sidebar:
     #     st.success('API key already provided!', icon='✅')
     #     replicate_api = st.secrets['REPLICATE_API_TOKEN']
     # else:
-    # replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    # if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-    #     st.warning('Please enter your credentials!', icon='⚠️')
-    # else:
-    #     st.success('Proceed to entering your prompt message!', icon='👉')
-    replicate_api = "r8_RylgPiJn3R7qK9Bq3txiGu2Y6hOH0lv0URHrL"
+    #     replicate_api = st.text_input('Enter Replicate API token:', type='password')
+    #     if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+    #         st.warning('Please enter your credentials!', icon='⚠️')
+    #     else:
+    #         st.success('Proceed to entering your prompt message!', icon='👉')
+    replicate_api = 'r8_RylgPiJn3R7qK9Bq3txiGu2Y6hOH0lv0URHrL'
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
@@ -31,8 +28,7 @@ with st.sidebar:
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=600, step=8)
-
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=500, value=500, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
